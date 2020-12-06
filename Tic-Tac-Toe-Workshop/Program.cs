767246b8012be7ebe37c379032557844741cdc00
Ramya.U <ramyayadav2016@gmail.com>
2020-12-06 19:14:15 +0530

    [Ramya] Add . Added method to check whether game is win or tie

--- Tic-Tac-Toe-Workshop/Program.cs
+++ Tic-Tac-Toe-Workshop/Program.cs
@@ -11,6 +11,16 @@ namespace Tic_Tac_Toe_Workshop
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Welcome to Tic-Tac-Toe!");
+            TicTacToeGame game = new TicTacToeGame();
+            game.CreateBoard();
+            char userChoice = game.Choice();
+            char compChoice;
+            if (userChoice == 'X')
+                compChoice = 'O';
+            else
+                compChoice = 'X';
+            game.ShowBoard();
+            game.Play(userChoice, compChoice);
         }
     }
 }
