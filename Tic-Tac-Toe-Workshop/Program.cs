674a81cfc3bd9dbb427fe670c534fbf4835cc04b
Ramya.U <ramyayadav2016@gmail.com>
2020-12-06 18:40:20 +0530

    [Ramya] Add . Added method to create board

--- Tic-Tac-Toe-Workshop/Program.cs
+++ Tic-Tac-Toe-Workshop/Program.cs
@@ -11,6 +11,8 @@ namespace Tic_Tac_Toe_Workshop
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Welcome to Tic-Tac-Toe!");
+            TicTacToeGame game = new TicTacToeGame();
+            game.CreateBoard();
         }
     }
 }
---
+++ Tic-Tac-Toe-Workshop/TicTacToe.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tic_Tac_Toe_Workshop
+{
+    public class TicTacToeGame
+    {
+        public char[] board;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TicTacToeGame"/> class.
+        /// </summary>
+        public TicTacToeGame()
+        {
+            board = new char[10];
+        }
+
+        /// UC1
+        /// <summary>
+        /// Creates the board.
+        /// </summary>
+        public void CreateBoard()
+        {
+            TicTacToeGame game = new TicTacToeGame();
+            for (int position = 1; position < 10; position++)
+            {
+                game.board[position] = ' ';
+            }
+        }
+    }
+}
